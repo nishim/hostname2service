26710fcb8a3871dd4bee865f1a5d955ee765d134
Yuji Nishimura <nishimura.yuji@liclab.com>
2020-08-20 12:09:02 +0900

    add aws

--- h2s.go
+++ h2s.go
@@ -1,6 +1,19 @@
 package h2s
 
+import "strings"
+
 // Detect service name by hostname.
 func Detect(hostname string) string {
+	tailMatch := map[string]string{
+		".compute.amazonaws.com": "AWS EC2",
+		".cloudfront.net":        "AWS CloudFront",
+	}
+
+	for k, v := range tailMatch {
+		if strings.HasSuffix(hostname, k) {
+			return v
+		}
+	}
+
 	return "undefined"
 }
--- h2s_test.go
+++ h2s_test.go
@@ -9,3 +9,20 @@ func TestUndefined(t *testing.T) {
 		t.Errorf("expected: %v, actual: %v", e, a)
 	}
 }
+
+func TestDefined(t *testing.T) {
+	cases := []struct {
+		service string
+		name    string
+	}{
+		{service: "AWS EC2", name: "ec2-0-0-0-0.ap-northeast-1.compute.amazonaws.com"},
+		{service: "AWS CloudFront", name: "server-0-0-0-0.nrt00.r.cloudfront.net"},
+	}
+
+	for _, c := range cases {
+		s := Detect(c.name)
+		if s != c.service {
+			t.Errorf("expected: %v, actual: %v", c.service, s)
+		}
+	}
+}
